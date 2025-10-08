809b8df325b81e575a235d846551ba6ced19e609
Eryck Assis <eng.assis.dev@gmail.com>
2025-10-08 11:56:10 -0300

    inicio de integração do Typescript e a remoção do PropTypes

---
+++ src/types/card.types.ts
@@ -0,0 +1,24 @@
+export interface CardProfileData {
+  name: string;
+  description: string;
+  followers: number;
+  dark: boolean;
+  imageSource: string;
+}
+
+export interface CardProfileProps extends CardProfileData {
+  isFollowing: boolean;
+  onToggle: () => void;
+}
+
+export interface CardProfileCallbacks {
+  onToggle: () => void;
+  onImageLoad?: () => void;
+  onImageError?: (error: Error) => void;
+}
+
+export interface CardProfileConfig {
+  showFollowerCount: boolean;
+  allowToggle: boolean;
+  theme: "ligth" | "dark" | "auto";
+}
